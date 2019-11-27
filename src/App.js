9ce87b73d0e0586c7c9834472aa9bd9bfe6eeaeb
yangchch6 <ycc54140127@163.com>
2019-11-27 10:59:05 +0800

    react demo 工程

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
 import React, { Component } from 'react';
-// import TaskApp from './task-app';
-import Message from 'bee-message';
-import { Button } from 'tinper-bee';
-// import Switch from 'bee-switch';
-// import { message, Button } from 'antd';
-// import "antd/dist/antd.css";
-// import "tinper-bee/assets/theme/tinper-bee-indigo.css";
-
-// const info = () => {
-//     message.info('This is a normal message');
-//     setTimeout(()=>{
-//         console.log('antd 重置')
-//         message.config({
-//             top: 200,
-//             duration: 10,
-//             maxCount: 3,
-//         });
-//     },2000)
-// };
-
-const info = () => {
-    Message.create({content: '单据状态更新，请在审批中心查看最新状态。', color: 'info'});
-    setTimeout(()=>{
-        console.log('tinper-bee 重置')
-        Message.config({
-            top: 350,
-            duration: 10,
-        });
-    },2000)
-};
+import ColorPicker from "bee-colorpicker";
+import "bee-colorpicker/build/ColorPicker.css";
 
 export default class App extends Component{
-    state={
+    state = {
+        value : "#E14C46"
     }
 
+    handleChange = (v) => {
+        console.log("选择的色彩信息 ：" , v);
+        this.setState({
+            value: v.hex || ''
+        })
+    }
     render(){
         return (
             <div>
-                <Button colors="primary" onClick={info}>
-                    Display normal message
-                </Button>
+                <ColorPicker 
+                    label="颜色"
+                    placeholder="请输入十六进制色值"
+                    value={this.state.value} 
+                    onChange={this.handleChange}
+                    disabledModal={true}
+                />
             </div>
         )
     }
